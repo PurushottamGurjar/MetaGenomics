96260b3a816c7dd1371bf56ae96e445a71d40bde
PurushottamGurjar <purushottamnitwarangal@gmail.com>
2025-12-29 02:56:10 +0530

    minor update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api=axios.create({
-    baseURL:"https://metagenomics-backend.onrender.com"
+    baseURL:"https://metagenomics-backend.onrender.com/api"
 });
 
 api.interceptors.request.use((config)=>{
